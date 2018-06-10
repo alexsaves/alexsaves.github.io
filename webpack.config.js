18547ac79c72d0b267c13206f1e1a6ad4c40f778
Alexei White <white@Alexeis-MacBook-Pro.local>
2018-06-10 11:00:04 -0700

    Folder structure

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+var paths = {
+    APP_SRC: './src/js/',
+    DIST: './static/app/'
+};
+
+module.exports = {
+    entry: paths.APP_SRC + 'index.jsx',
+    output: {
+        path: path.resolve(__dirname, paths.DIST),
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            { test: /\.(js|jsx)$/, use: 'babel-loader' }
+        ]
+    }
+};
